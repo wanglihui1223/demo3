e13f6fcc025f6445638fe33f45432d60d1b28343
test-java1 <test-java@com.cn>
2020-04-01 13:17:31 +0800

    增加了一个id属性 --test-java1

--- src/main/java/com/newer/domain/Person.java
+++ src/main/java/com/newer/domain/Person.java
@@ -1,5 +1,14 @@
 package com.newer.domain;
 
 public class Person {
+	private Integer id;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
 }
