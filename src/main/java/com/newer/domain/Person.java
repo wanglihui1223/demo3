209a8850520b0715e1e734e268e3bc7cd878715e
wanglihui1223 <62825964+wanglihui1223@users.noreply.github.com>
2020-04-01 11:30:17 +0800

    person增加name属性 -----test-java2

--- src/main/java/com/newer/domain/Person.java
+++ src/main/java/com/newer/domain/Person.java
@@ -1,5 +1,15 @@
 package com.newer.domain;
 
 public class Person {
-
+	private String name;
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
